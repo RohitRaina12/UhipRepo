13716d09da08a53bf98afaa02022e584a5f3d367
rohit raina <rohit raina@DELL>
2019-11-15 03:02:39 +0530

    Initial commit
    Project is added to git

---
+++ UHIP/src/test/java/com/uhip/nj/gov/UhipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uhip.nj.gov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UhipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
